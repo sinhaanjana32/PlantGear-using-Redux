064c0f43b4a922704be0e3c6a8f0da637b165030
Anjana <sinhaanjana32@gmail.com>
2020-07-23 01:30:38 +0300

    Updates

---
+++ precache-manifest.7934f9f49175c945f267f494fbd52f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd48c94b55707ecdd8ea28d36af5e84",
+    "url": "/PlantGear-using-Redux/index.html"
+  },
+  {
+    "revision": "814321ea55d5ffc39152",
+    "url": "/PlantGear-using-Redux/static/css/main.52b07ff3.chunk.css"
+  },
+  {
+    "revision": "a616232645d72db27bc4",
+    "url": "/PlantGear-using-Redux/static/js/2.94e16cd6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/PlantGear-using-Redux/static/js/2.94e16cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814321ea55d5ffc39152",
+    "url": "/PlantGear-using-Redux/static/js/main.36ca39a9.chunk.js"
+  },
+  {
+    "revision": "1b3cedc101393167092c",
+    "url": "/PlantGear-using-Redux/static/js/runtime-main.ab3245ec.js"
+  }
+]);
